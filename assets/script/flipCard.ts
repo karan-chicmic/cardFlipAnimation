d1605cdb670170dc3481ed34e9ea1dffa58d03e6
Abhishek Rawat <abhishek.rawat@chicmic.co.in>
2024-04-18 14:32:07 +0530

    coming fine rest working wrong

--- assets/script/flipCard.ts
+++ assets/script/flipCard.ts
@@ -10,24 +10,40 @@ export class flipCard extends Component {
     count: number;
 
     protected start(): void {
+        this.onFlipStart();
+    }
+
+    onFlipStart() {
         for (let i = 0; i < 7; i++) {
             const card = instantiate(this.cardPrefab);
+            this.cardArray.push(card);
 
             card.setPosition(-750 + i * 200, 0, 0);
             setTimeout(() => {
                 let animation = card.getComponent(Animation);
                 this.node.addChild(card);
                 animation.play("flip");
-                animation.on(
-                    Animation.EventType.FINISHED,
-                    () => {
-                        animation.play("removeCard");
-                    },
-                    this
-                );
+                if (i == 6) {
+                    animation.on(
+                        Animation.EventType.FINISHED,
+                        this.onFlipFinished,
+
+                        this
+                    );
+                }
             }, i * 1000);
         }
     }
 
+    onFlipFinished() {
+        console.log("onFlipFinished console", this.node);
+        for (let index = 0; index < this.cardArray.length; index++) {
+            let card = this.cardArray[index];
+            let animation = card.getComponent(Animation);
+            Promise.resolve(animation.play("removeCard")).then(() => {
+                this.node.removeChild(card);
+            });
+        }
+    }
     update(deltaTime: number) {}
 }
